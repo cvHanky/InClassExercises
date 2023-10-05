cffbd716b7f0628be2c723ad5b97a815168bd439
Daniel Hoppe Hansen <danielhoppe123@gmail.com>
2023-10-05 11:35:13 +0200

    Added Pr14 loops and test

---
+++ Kode til opgaver/Pr14 loops/IntArrayHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr14_loops
+{
+    public class IntArrayHelper
+    {
+
+
+        public void SortAscending(int[] intArray)
+        {
+            int temp = 0;
+
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                for (int j = 1+i;j < intArray.Length; j++)
+                {
+                    if (intArray[i] > intArray[j])
+                    {
+                        temp = intArray[i];
+                        intArray[i] = intArray[j];
+                        intArray[j] = temp;
+                    }
+                }
+            }
+        }
+
+        public void SortAscendingAndReverse(int[] intArray)
+        {
+            int temp = 0;
+
+            for (int i = intArray.Length-1; i >= 0; i--)
+            {
+                for (int j = i-1; j >= 0; j--)
+                {
+                    if (intArray[i] > intArray[j])
+                    {
+                        temp = intArray[i];
+                        intArray[i] = intArray[j];
+                        intArray[j] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Kode til opgaver/Pr14 loops/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Pr14_loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IntArrayHelper intArrayHelper = new IntArrayHelper();
+            
+            int[] intArray = { 6, 1, 7, 2, 20, 4 };
+            intArrayHelper.SortAscending(intArray);
+
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                Console.WriteLine(intArray[i]);
+            }
+            intArrayHelper.SortAscendingAndReverse(intArray);
+
+            Console.WriteLine("\n\n");
+
+            for (int i = 0; i < intArray.Length; i++)
+                Console.WriteLine(intArray[i]);
+
+            Console.ReadKey();
+        }
+    }
+}
