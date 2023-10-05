5fa53ace24bbcdd087580cac5be5a605f24eedce
Daniel Hoppe Hansen <danielhoppe123@gmail.com>
2023-10-05 10:18:09 +0200

    Added bank account part of Pr14.

--- Kode til opgaver/Pr14 bankaccount/BankAccount.cs
+++ Kode til opgaver/Pr14 bankaccount/BankAccount.cs
@@ -8,6 +8,53 @@ namespace Pr14_bankaccount
 {
     public class BankAccount
     {
+		private string name;
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+		private double balance;
+		public double Balance
+		{
+			get { return balance; }
+		}
+		private bool locked;
 
+		public BankAccount(string name, double balance, bool locked)
+        {
+            Name = name;
+            this.balance = balance;
+            this.locked = locked;
+        }
+		public BankAccount(string name, double balance) : this(name, balance, false)
+        { 
+		}
+        public BankAccount(double balance) : this("", balance, false)
+        {
+        }
+		public void Deposit(double amount)
+		{
+			if (locked == false)
+				balance += amount;
+			else
+				Console.WriteLine("The account is currently locked");
+		}
+		public void Withdraw(double amount)
+		{
+			if (locked == false && balance >= amount)
+				balance -= amount;
+		}
+		public void ChangeLockState()
+		{
+			if (locked)
+				locked = false;
+			else if (locked == false)
+				locked = true;
+		}
+        public override string ToString()
+        {
+            return "Name: " + name + ", Balance: " + balance;
+        }
     }
 }
