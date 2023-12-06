b52519249bb565625c1d0d2fe0c20b2db20f5cba
cvHanky <145098299+cvHanky@users.noreply.github.com>
2023-12-06 10:44:50 +0100

    Finished bonus exercise Pr34 sparegris

---
+++ Kode til opgaver/Pr34 Sparegris bonus/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Pr34_Sparegris_bonus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sparegris piggy = new Sparegris();
+            piggy.AmountChanged += piggy.Update;
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine($"Welcome to piggy!\nPiggy's current amount is: {piggy.Amount}\n\nPlease insert an amount: ");
+                if (double.TryParse(Console.ReadLine(), out double result))
+                {
+                    piggy.InsertAmount(result);
+                    Console.WriteLine("Press any button to continue...");
+                    Console.ReadKey();
+                }
+
+            }
+        }
+    }
+}
---
+++ Kode til opgaver/Pr34 Sparegris bonus/Sparegris.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr34_Sparegris_bonus
+{
+	public delegate void SparegrisBonusEventHandler(object sender, EventArgs e);
+    public class Sparegris
+    {
+		private double amount;
+		public double Amount
+		{
+			get { return amount; }
+			set { amount = value; OnAmountChanged(); }
+		}
+		public Sparegris()
+		{
+			amount = 0;
+		}
+
+		public event SparegrisBonusEventHandler AmountChanged;
+		public void OnAmountChanged()
+		{
+			if (AmountChanged != null)
+			{
+				AmountChanged(this,null);
+			}
+		}
+		public void InsertAmount(double amount)
+		{
+			Amount += amount;
+		}
+		public void Update(object sender, EventArgs e)
+		{
+			if (sender is Sparegris piggy)
+			{
+                Console.WriteLine($"New amount is: {piggy.amount}");
+
+				if (piggy.amount >= 399 && piggy.amount < 799)
+                    Console.WriteLine("Tillyke - du har nu penge nok til et sæsonkort til OB's kampe.");
+                else if (piggy.amount >= 799 && piggy.amount < 1197)
+                    Console.WriteLine("Tillyke - du har nu penge nok til et premium sæsonkort til OB's kampe.");
+                else if (piggy.amount >= 1197)
+                    Console.WriteLine("Tillyke - du har nu penge nok til tre sæsonkort til OB's kampe.");
+			}
+		}
+	}
+}
